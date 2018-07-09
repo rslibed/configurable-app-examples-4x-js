c6757fc3a753121f711ca592f8d6ab6d37ad72b5
Ryan Libed <ryan0000@esri.com>
2018-07-09 11:56:06 -0700

    Added comments to functionality of methods. Changed variable names

--- webmap-ts/app/Share/ShareWidget.js
+++ webmap-ts/app/Share/ShareWidget.js
@@ -90,31 +90,31 @@ define(["require", "exports", "esri/core/tsSupport/declareExtendsHelper", "esri/
             //----------------------------------
             //----------------------------------
             //
-            //  loading - readOnly
+            //  loading
             //
             //----------------------------------
             _this.loading = null;
             //----------------------------------
             //
-            //  shareUrl - readOnly
+            //  shareUrl
             //
             //----------------------------------
             _this.shareUrl = null;
             //----------------------------------
             //
-            //  shortenedUrl - readOnly
+            //  shortenedUrl
             //
             //----------------------------------
             _this.shortenedUrl = null;
             //----------------------------------
             //
-            //  geometryServiceUrl - readOnly
+            //  geometryServiceUrl
             //
             //----------------------------------
-            _this.geometryServiceurl = null;
+            _this.geometryServiceUrl = null;
             //----------------------------------
             //
-            //  linkGenerated - readOnly
+            //  linkGenerated
             //
             //----------------------------------
             _this.linkGenerated = null;
@@ -175,10 +175,10 @@ define(["require", "exports", "esri/core/tsSupport/declareExtendsHelper", "esri/
                 ? i18n.clipboard
                 : i18n.generateLink;
             var generateLinkNode = (widget_1.tsx("button", { bind: this, onclick: this._shortenShareUrl, onkeydown: this._shortenShareUrl, tabIndex: 0, class: CSS.main.mainShorten.shortenUrl }, i18n.generateLink));
+            var copyShortenNode = this.linkGenerated ? (widget_1.tsx("input", { class: CSS.main.mainUrl.urlInput, bind: this, onclick: this._copyUrl, onkeydown: this._copyUrl, tabindex: 0, type: "text", value: this.shortenedUrl ? this.shortenedUrl : this.shareUrl, afterCreate: widget_1.storeNode, "data-node-ref": "_urlNode", readOnly: true })) : (generateLinkNode);
             var loadingIconNode = this.shortenLinkEnabled ? (this.loading ? (widget_1.tsx("div", { class: CSS.main.mainShorten.loading },
                 widget_1.tsx("svg", { xmlns: "http://www.w3.org/2000/svg", width: "32", height: "32", viewBox: "0 0 32 32", class: this.classes(CSS.icons.svgIcon, CSS.icons.esriRotatingIcon, CSS.icons.loadingIcon) },
                     widget_1.tsx("path", { d: "M27.518 8.338c.324.37.772.94 1.261 1.727a13.499 13.499 0 0 1 1.986 7.41c-.019 3.243-1.41 7.185-4.559 10.081-3.085 2.902-7.94 4.492-12.611 3.566-4.697-.832-8.864-4.161-10.853-8.38-2.043-4.23-1.863-9.035-.373-12.647 1.463-3.672 4.051-6.09 6.098-7.421C10.551 1.336 12.092.889 12.389.802c1.234-.356 2.457-.18 3.282.309.839.511 1.281 1.259 1.276 2.105-.079 1.717-1.406 3.039-2.86 3.478-.19.051-1.158.258-2.564.99a10.6 10.6 0 0 0-4.43 4.522c-1.216 2.318-1.698 5.672-.504 8.872 1.158 3.185 4.042 6.059 7.693 7.058 3.629 1.078 7.773.199 10.671-2.06 2.944-2.244 4.563-5.648 4.855-8.66.369-3.046-.465-5.615-1.261-7.222a13.163 13.163 0 0 0-1.084-1.812l-.45-.601.504.559z" })))) : null) : null;
-            var copyShortenNode = this.linkGenerated ? (widget_1.tsx("input", { class: CSS.main.mainUrl.urlInput, bind: this, onclick: this._copyUrl, onkeydown: this._copyUrl, tabindex: 0, type: "text", value: this.shortenedUrl ? this.shortenedUrl : this.shareUrl, afterCreate: widget_1.storeNode, "data-node-ref": "_urlNode", readOnly: true })) : (generateLinkNode);
             return (widget_1.tsx("div", { class: CSS.base },
                 widget_1.tsx("div", { class: CSS.header.container },
                     widget_1.tsx("h1", { class: CSS.header.heading }, i18n.heading),
@@ -232,9 +232,7 @@ define(["require", "exports", "esri/core/tsSupport/declareExtendsHelper", "esri/
         };
         Share.prototype._openUrl = function (url, title, summary, urlTemplate) {
             var urlToOpen = lang_1.substitute({
-                url: encodeURI(
-                // "http://www.arcgis.com/apps/PublicInformation/index.html"
-                url),
+                url: encodeURI(url),
                 title: title,
                 summary: summary
             }, urlTemplate);
@@ -251,8 +249,8 @@ define(["require", "exports", "esri/core/tsSupport/declareExtendsHelper", "esri/
             var shareIcons = CSS.main.mainShare.shareIcons;
             // Assign class names of icons to share item
             shareItems.items.forEach(function (shareItem) {
-                for (var key in shareIcons) {
-                    if (key === shareItem.id) {
+                for (var icon in shareIcons) {
+                    if (icon === shareItem.id) {
                         shareItem.className = shareIcons[shareItem.id];
                     }
                 }
@@ -276,7 +274,7 @@ define(["require", "exports", "esri/core/tsSupport/declareExtendsHelper", "esri/
         __decorate([
             decorators_1.aliasOf("viewModel.geometryServiceUrl"),
             widget_1.renderable()
-        ], Share.prototype, "geometryServiceurl", void 0);
+        ], Share.prototype, "geometryServiceUrl", void 0);
         __decorate([
             decorators_1.aliasOf("viewModel.linkGenerated"),
             widget_1.renderable()
